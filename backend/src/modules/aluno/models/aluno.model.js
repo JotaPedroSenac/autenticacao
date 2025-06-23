0685b6b0795c5dfc5b96e2e9a1c05c6f03d24527
JOAO PEDRO DE SOUZA CORINGA <a95436285@rn.senac.local>
2025-06-23 17:30:17 -0300

    feat: aula autenticacao part 2

--- backend/src/modules/aluno/models/aluno.model.js
+++ backend/src/modules/aluno/models/aluno.model.js
@@ -1,49 +1,52 @@
 const { DataTypes } = require('sequelize');
 const { Sequelize } = require('../../../config/configDB');
 
-const Aluno = sequelize.define(
-    'Aluno',
-    {
-      // Model attributes are defined here
-      nome: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      matricula: {
-        type: DataTypes.STRING,
-        primaryKey: true,
-        validate: {
-            is:{
-                args: /^[A-Za-z]\d{8}$/,
-                msg: 'A matricula deve iniciar com uma letra e 8 numeros'
-            }
-        }
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate:{
-            isEmail:{
-                msg: "Email inválido"
-            }
-        }
-      },
-      senha: DataTypes.STRING,
+const Aluno = Sequelize.define(
+  'Aluno',
+  {
+    // Model attributes are defined here
+    nome: {
+      type: DataTypes.STRING,
       allowNull: false,
-      validate:{
-        is:{
-            args: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-            msg: 'A senha deve ter no mínimo 8 caracteres, com ao menos uma letra maiúscula, uma minúscula, um número e um caractere especial (@$!%*?&).'
-
+    },
+    matricula: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      validate: {
+        is: {
+          args: /^[A-Za-z]\d{8}$/,
+          msg: 'A matricula deve iniciar com uma letra e 8 numeros'
         }
       }
     },
-    {
-      modelName: 'aluno',
-      createdAt: 'criado_em',
-      updatedAt: 'atualizado_em'
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          msg: "Email inválido"
+        }
+      }
     },
-  );
+    senha: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        is: {
+          args: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+          msg: 'A senha deve ter no mínimo 8 caracteres, com ao menos uma letra maiúscula, uma minúscula, um número e um caractere especial (@$!%*?&).'
 
-  module.exports = Aluno;
+        }
+      }
+    }
+
+  },
+  {
+    modelName: 'aluno',
+    createdAt: 'criado_em',
+    updatedAt: 'atualizado_em'
+  },
+);
+
+module.exports = Aluno;
--- backend/src/modules/autenticacao/controller/autenticacao.controller.js
+++ backend/src/modules/autenticacao/controller/autenticacao.controller.js
@@ -17,8 +17,8 @@ class AutenticacaoController {
     };
 
     // gerar o refresh token
-    static gerarRefressToken(dadosUsuario) {
-        return jwt.sign(dadosUsuario, process.env.SECRET_KEY, {
+    static gerarRefressToken(dadosAluno) {
+        return jwt.sign(dadosAluno, process.env.SECRET_KEY, {
             expiresIn: tempo_refresh_token
         });
     };
@@ -27,26 +27,85 @@ class AutenticacaoController {
         try {
             const { matricula, senha } = req.body;
             if (!matricula || !senha) {
-                return res.status(400).json({msg: 'É necessário informar email e senha'})
+                return res.status(400).json({ msg: 'É necessário informar email e senha' })
             }
 
             const aluno = await Aluno.findOne({
-                where: {matricula}
+                where: { matricula }
             })
 
-            if(!aluno){
-                return res.status(401).json({msg: 'Aluno não encontrado'})
+            if (!aluno) {
+                return res.status(401).json({ msg: 'Aluno não encontrado' })
             }
-        } catch (error) {
 
+            const senhaCorreta = await bcrypt.compare(senha, aluno.senha);
+            if (!senhaCorreta) {
+                return res.status(400).json({ msg: 'E-mail ou senha estão incorretos' })
+            }
+
+            const dadosAluno = {
+                nome: aluno.nome,
+                papel: 'aluno'
+            }
+
+            // gerando os tokens
+            const tokenAcesso = AutenticacaoController.gerarTokenAcesso(dadosAluno);
+            const refreshToken = AutenticacaoController.gerarRefressToken(dadosAluno);
+
+            res.cookie("refreshToken", refreshToken, {
+                httpOnly: false,
+                secure: process.env.NODE_ENV,
+                sameStrict: 'strict',
+                maxAge: 1 * 24, // 1dia
+            })
+
+            res.status(200).json({
+                msg: 'Aluno logado com sucesso',
+                tokenAcesso,
+                nome: usuario.nome,
+                papel: "aluno",
+            });
+
+        } catch (error) {
+            res.status(500).json({ msg: 'Erro interno do servidor. Por favor tente mais tarde.', erro: error.message })
         }
+    }
+
+    // Método para renovar o refresh token
+    static refreshToken(req, res) {
+        const { refreshToken } = req.cookies;
+        if (!refreshToken) {
+            return res.status(403).json({ msg: "Refresh token invalido!" });
+        }
+        jwt.verify(
+            refreshToken,
+            process.env.JWT_REFRESH_SECRET,
+            (erro, usuario) => {
+                if (erro) {
+                    return res.status(403).json({ msg: "Refresh invalido!" });
+                }
+                const dadosAluno = {
+                    matricula: usuario.matricula,
+                    nome: usuario.nome,
+                    papel: "admin",
+                };
+                // gerando o novo token
+                const novoTokenAcesso = this.gerarTokenAcesso(dadosAluno);
+                // atualizando o token antigo para o novo
+                res.status(200).json({ tokenAcesso: novoTokenAcesso });
+            }
+        );
     }
 
     static async sair(req, res) {
         try {
-
+            res.clearCookies("refreshToken", {
+                httpOnly: false,
+                secure: process.env.NODE_ENV,
+                sameStrict: "strict",
+              });
         } catch (error) {
-
+            res.status(500).json({msg: 'Erro interno do servidor', erro: error.message})
         }
     }
 }
